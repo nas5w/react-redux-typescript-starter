127c5bdb67034a1814ed70d85e941481ccadcf6d
Nick <7538045+nas5w@users.noreply.github.com>
2019-10-26 14:17:25 -0400

    Add redux devtools

--- src/redux/create.ts
+++ src/redux/create.ts
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import widgetReducer, { WidgetAction } from "./modules/widgets";
 import { StateType } from "typesafe-actions";
@@ -7,7 +7,14 @@ const rootReducer = combineReducers({
   widgets: widgetReducer
 });
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export type RootAction = WidgetAction;
 export type Store = StateType<typeof store>;
